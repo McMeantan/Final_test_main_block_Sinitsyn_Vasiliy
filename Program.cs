17d4a350aa5cafcce1a7113fb9b6816351392d89
mcmeantan <russky1@yandex.ru>
2025-06-18 21:25:23 +0300

    Program totally modified

--- Program.cs
+++ Program.cs
@@ -1,60 +1,24 @@
-﻿void EnterArray()
-{
-    Console.Write("Введите количество строк, которые хотите ввести: ");
-    int n = Convert.ToInt32(Console.ReadLine()); // Считываем строку, переводим в число
-    string[] strs = new string[n]; // Объявляем массив строк длиной n (которую ввёл пользователь)
-    for (int i = 0; i < n; i++)
-        {
-            Console.Write("Введите строку №{0}:\r\n ", i + 1);
-            strs[i] = Console.ReadLine(); // Заполняем его
-        }
-    Console.WriteLine("Вы ввели следующие строки:");
-    for (int i = 0; i < n; i++)
-        {
-            Console.WriteLine(strs[i]);
-        }
-    Console.Write("Нажмите [Enter] для выхода...");
-    Console.ReadLine();
-}
+﻿Console.WriteLine("Введите строки через пробел:");
 
-string[] Select3OrLess(string[] s, int size)
-{
-   string[] array = new string[size];
+string input = Console.ReadLine();
 
-    for (int i = 0; i < size; i++)
+string[] inputArray = input.Split(' ');
+
+List<string> resultList = new List<string>();
+
+foreach (string str in inputArray)
+{
+    if (str.Length <= 3)
     {
-        for (int j = 0; j < s.Length; j++)
-        {
-            if (array[i].Length <= 3)
-            {
-                s[j] = array[i];
-            }
-        } 
+        resultList.Add(str);
     }
-
-    return s;
 }
 
-void PrintArray(string[] s)
-{
-    Console.Write("[");
+string[] resultArray = resultList.ToArray();
 
-    for (int i = 0; i < s.Length; i++)
-    {
-        if (i < s.Length - 1)
+Console.WriteLine("Строки длиной <= 3:");
+
+        foreach (string str in resultArray)
         {
-            Console.Write($"{s[i]}, ");
+            Console.WriteLine(str);
         }
-        else
-        {
-            Console.Write($"{s[i]}");
-        }
-    }
-
-    Console.Write("]");
-}
-
-massiv = EnterArray();
-
-string[] resArray = Select3OrLess(massiv, 10);
-PrintArray(resArray);
